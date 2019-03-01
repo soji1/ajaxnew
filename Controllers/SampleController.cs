80a5e5ae87fbfe464911f166fbe62f7a30cd5f43
soji1 <33921879+soji1@users.noreply.github.com>
2019-03-01 09:11:50 +0530

    Add files via upload

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace clinicapp.Controllers
+{
+    public class SampleController : Controller
+    {
+        //
+        // GET: /Sample/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult getString()
+        {
+            return View();
+        }
+        public ActionResult getStudent()
+        {
+            return View();
+        }
+	}
+}
---
+++ Models/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace clinicapp.Models
+{
+    public class Student
+    {
+    [Required(ErrorMessage="cant be empty")]
+        [Display(Name="Student Name")]
+       // [RegularExpression()]
+        public string Sname { get; set; }
+    [Required(ErrorMessage = "cant be empty")]
+    [Display(Name = "Student Address")]
+        [DataType(DataType.MultilineText)]
+        public string Saddress { get; set; }
+        [Required(ErrorMessage = "cant be empty")]
+        [Display(Name="Student Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Semail { get; set; }
+        public string Gender  { get; set; }
+        public Course CourseList { get; set; }
+        [Required(ErrorMessage = "cant be empty")]
+        [Display(Name = "Student UserName")]
+        // [RegularExpression()]
+        public string Susername{ get; set; }
+        [Required(ErrorMessage = "8 characters")]
+        [Display(Name = "Student Password")]
+        // [RegularExpression()]
+        public string Spassword{ get; set; }
+        [Required(ErrorMessage = "8 characters")]
+        [Display(Name = "Student Confirmpassword")]
+        // [RegularExpression()]
+        public string Sconfirmpassword{ get; set; }
+
+    }
+   
+    public enum Course
+    {
+        MCA,
+        BCA,
+        Mtech
+    }
+}
